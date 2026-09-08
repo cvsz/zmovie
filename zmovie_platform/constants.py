51646566e4256786f54c9131b359698be567cdb1
cvsz <seaza@msn.com>
2026-09-08 19:38:40 +0700

    feat: add production option constants

---
+++ zmovie_platform/constants.py
@@ -0,0 +1,2 @@
+SUPPORTED_ASPECT_RATIOS = ("16:9", "9:16", "1:1", "4:3", "21:9")
+SUPPORTED_GENRES = ("action", "thriller", "drama", "comedy", "sci-fi", "fantasy", "horror", "romance", "documentary", "commercial")
